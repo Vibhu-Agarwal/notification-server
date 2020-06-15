58b01bc1013bfe41bf44347a9710b1a7fde9b82a
Vibhu Agarwal <vibhu4agarwal@gmail.com>
2020-06-15 20:31:31 +0530

    Register Notifications in admin panel

--- src/notifications/admin.py
+++ src/notifications/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Notification
 
-# Register your models here.
+
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'url', 'method', 'time')
+    list_display_links = ('id', 'url', 'method', 'time')
+
+
+admin.site.register(Notification, NotificationAdmin)
