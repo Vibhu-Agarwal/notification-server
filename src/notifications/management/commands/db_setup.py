a75ac0dcd048f85041d79ca0ae8f0b030073b1bb
Vibhu Agarwal <vibhu4agarwal@gmail.com>
2020-06-15 18:05:47 +0530

    Added DB-Setup Command

---
+++ src/notifications/management/commands/db_setup.py
@@ -0,0 +1,31 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+from django.conf import settings
+
+User = get_user_model()
+
+email_superuser = 'vibhu4agarwal@gmail.com'
+superuser_username = 'superuser'
+superuser_phone_number = 9999999999
+
+help_message = f"""
+Sets up the DB, creating:
+1) superuser with admin rights (Email: {email_superuser})
+"""
+
+
+class Command(BaseCommand):
+    """
+    db_setup: Command to set-up database for the application
+    """
+    help = help_message
+
+    def handle(self, *args, **kwargs):
+        if not User.objects.filter(username=superuser_username).exists():
+            User.objects.create_superuser(username=superuser_username,
+                                          first_name="Super User",
+                                          email=email_superuser,
+                                          password=settings.SUPERUSER_PASSWORD,
+                                          phone_number=superuser_phone_number)
+            print('Super-User Created!')
+        print('Notification-Server DB Set-Up Complete!')
