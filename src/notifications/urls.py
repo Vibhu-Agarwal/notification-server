5a8972a7c6f1c753e2ad1d6e4823bf411002609e
Vibhu Agarwal <vibhu4agarwal@gmail.com>
2020-06-16 00:29:42 +0530

    making requests with notifications

--- src/notifications/urls.py
+++ src/notifications/urls.py
@@ -1,5 +1,4 @@
-from .views import (CreateNotificationAPIView, DeleteNotificationAPIView,
-                    TestAPIView)
+from .views import (CreateNotificationAPIView, DeleteNotificationAPIView)
 
 from django.urls import path
 
@@ -8,5 +7,4 @@ app_name = 'notifications'
 urlpatterns = [
     path('place/', CreateNotificationAPIView.as_view(), name='place_notification'),
     path('remove/<int:pk>', DeleteNotificationAPIView.as_view(), name='remove_notification'),
-    path('test/', TestAPIView.as_view(), name='test_notification'),
 ]
--- src/notifications/views.py
+++ src/notifications/views.py
@@ -1,16 +1,51 @@
+import json
+import requests
 from .models import Notification
 from .serializers import NotificationSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from background_task import background
-from rest_framework.generics import CreateAPIView, DestroyAPIView
+from rest_framework.generics import DestroyAPIView
+
+REQUEST_FUNCTIONS = {
+    'post': requests.post,
+    'delete': requests.delete,
+    'patch': requests.patch,
+    'get': requests.get
+}
+
+@background()
+def notify(notification_id):
+    notifications = Notification.objects.filter(id=notification_id)
+    if notifications.exists():
+        notification = notifications.first()
+        fn = REQUEST_FUNCTIONS.get(notification.method)
+        if notification.method in REQUEST_FUNCTIONS:
+            if notification.method in ['post', 'patch']:
+                data = notification.data
+                if data is None:
+                    data = '{}'
+                fn(notification.url, data=data)
+            elif notification.method in ['get', 'delete']:
+                fn(notification.url)
+            notification.delete()
 
 
-class CreateNotificationAPIView(CreateAPIView):
+class CreateNotificationAPIView(APIView):
 
-    queryset = Notification.objects.all()
-    serializer_class = NotificationSerializer
     permission_classes = ()
+
+    def post(self, request, *args, **kwargs):
+        request_body = request.data
+        data = request_body.get('data', None)
+        if data:
+            data = json.dumps(data)
+        request_body.update({'data': data})
+        notification_serializer = NotificationSerializer(data=request_body)
+        notification_serializer.is_valid(raise_exception=True)
+        notification = notification_serializer.save()
+        notify(notification.id, schedule=notification.time)
+        return Response(notification_serializer.data)
 
 
 class DeleteNotificationAPIView(DestroyAPIView):
@@ -18,15 +53,3 @@ class DeleteNotificationAPIView(DestroyAPIView):
     queryset = Notification.objects.all()
     serializer_class = NotificationSerializer
     permission_classes = ()
-
-
-@background()
-def notify():
-    print('WEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE')
-
-
-class TestAPIView(APIView):
-
-    def get(self, request, *args, **kwargs):
-        notify(schedule=90)
-        return Response({})
