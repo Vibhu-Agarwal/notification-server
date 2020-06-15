adca3ca5d9fde452d0b383e3c63714418f98e04a
Vibhu Agarwal <vibhu4agarwal@gmail.com>
2020-06-15 20:31:14 +0530

    Added apps in settings

--- src/notification_server/settings.py
+++ src/notification_server/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'notifications'
+    'rest_framework',
+    'background_task',
+    'notifications',
 ]
 
 MIDDLEWARE = [
